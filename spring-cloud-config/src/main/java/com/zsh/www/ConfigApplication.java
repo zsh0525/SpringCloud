bc9ead8c1698112472cead7e145f9078e0c5bb39
zsh0525 <824637534@qq.com>
2021-12-26 18:48:36 +0800

    2021.12.26：    1.初始化项目

---
+++ spring-cloud-config/src/main/java/com/zsh/www/ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.zsh.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author
+ * @description
+ * @date
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
