d6b77ee26b934c54cf2e61ef7fbf5d182c6c63e7
zsh0525 <824637534@qq.com>
2021-12-28 18:11:22 +0800

    2021.12.28：    1.spring-cloud-config config刷新

--- spring-cloud-config/src/main/java/com/zsh/www/ConfigApplication.java
+++ spring-cloud-config/src/main/java/com/zsh/www/ConfigApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * @author
  * @description
