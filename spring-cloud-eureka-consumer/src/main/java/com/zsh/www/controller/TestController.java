2f2cbe15f26e4f5c955f77cec7756bb071b1c148
zsh0525 <824637534@qq.com>
2022-03-13 19:04:04 +0800

    2022.03.13：    1.SpringCloud eureka服务注册以及基于Feign远程调用

--- spring-cloud-eureka-consumer/src/main/java/com/zsh/www/controller/TestController.java
+++ spring-cloud-eureka-consumer/src/main/java/com/zsh/www/controller/TestController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
-
     @Autowired
     TestService testService;
 
